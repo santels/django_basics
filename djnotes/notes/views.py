a9c1355d0882f02f46c072c9124fc552a2347c83
santels <andrewsantelices@gmail.com>
2017-04-04 23:47:00 +0800

    templates, views and urls added

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class Index(View):
+    template_name = 'notes/index.html'
+
+    def get_context_data(self):
+        context = {}
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
