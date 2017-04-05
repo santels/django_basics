5abaa6cf52de81362f092edcb70684c905f97c50
Andrew <andrewsantelices@gmail.com>
2017-04-05 11:26:41 +0800

    updated views, templates and some data in db

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Note
 
 class Index(View):
     template_name = 'notes/index.html'
 
     def get_context_data(self):
-        context = {}
+        context = {
+                    'notes':Note.objects.order_by('-id'),
+                }
         return context
 
     def get(self, request, *args, **kwargs):
